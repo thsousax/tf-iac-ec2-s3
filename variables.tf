346b8360c73412895e9b93612848273c9e1c0c1a
Antonio Thiago <thiago.sousa@rededor.com.br>
2025-03-17 22:03:49 -0300

    ✨ (s3-bucket.tf, variables.tf): add Terraform configuration for AWS S3 bucket and define necessary variables for AWS resources to enable infrastructure as code setup

---
+++ variables.tf
@@ -0,0 +1,37 @@
+#==================================================================
+# variables.tf - Script de definicao de Variaveis
+#==================================================================
+
+#------------------------------------------------------------------
+# Aws Provider
+#------------------------------------------------------------------
+variable "aws_region" {
+  type        = string
+  description = "Região da conta Aws"
+}
+
+variable "aws_profile" {
+  type        = string
+  description = "Profile Aws CLI"
+}
+
+#------------------------------------------------------------------
+# EC2 Instance
+#------------------------------------------------------------------
+variable "instance_ami" {
+  type        = string
+  description = "Ami do EC2"
+}
+
+variable "instance_type" {
+  type        = string
+  description = "Tipo da EC2"
+}
+
+#------------------------------------------------------------------
+# S3 Bucket
+#------------------------------------------------------------------
+variable "bucket_name" {
+  type        = string
+  description = "Nome do Bucket"
+}
