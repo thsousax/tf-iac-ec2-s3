02441e66374d8c77c1bba43ca0d18892bbdf69d1
Antonio Thiago <thiago.sousa@rededor.com.br>
2025-03-17 22:53:08 -0300

    📝 (outputs.tf): reorganize output sections for better readability and clarity by adding headers and comments

--- outputs.tf
+++ outputs.tf
@@ -1,22 +1,30 @@
-# Output do IP público da instância EC2
-output "ec2_public_ip" {
-  description = "O IP público da instância EC2"
-  value       = aws_instance.this.public_ip
-}
-
-# Output do ID da instância EC2
+#------------------------------------------------------------------
+# Id Instance EC2
+#------------------------------------------------------------------
 output "ec2_instance_id" {
   description = "O ID da instância EC2"
   value       = aws_instance.this.id
 }
 
-# Output do nome do bucket S3
+#------------------------------------------------------------------
+# IP Public EC2
+#------------------------------------------------------------------
+output "ec2_public_ip" {
+  description = "O IP público da instância EC2"
+  value       = aws_instance.this.public_ip
+}
+
+#------------------------------------------------------------------
+# Bucket Name
+#------------------------------------------------------------------
 output "s3_bucket_name" {
   description = "Nome do bucket S3"
   value       = aws_s3_bucket.this.bucket
 }
 
-# Output da URL do bucket S3
+#------------------------------------------------------------------
+# URL Bucket S3
+#------------------------------------------------------------------
 output "s3_bucket_url" {
   description = "URL do bucket S3"
   value       = "https://${aws_s3_bucket.this.bucket}.s3.amazonaws.com/"
